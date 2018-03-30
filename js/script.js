c9afe59d43668e56d4d6faa35a4995241543f894
AIndoria <indoriaabhishek@gmail.com>
2018-03-30 01:41:47 -0700

    Changed layout from flexbox to grid+flexbox

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ getLocation();
 var temp=document.getElementById("temp");
 
 function loadAPI(){
-    var request=new XMLHttpRequest();
-    request.open("GET","xx");
-    request.onload=function(){
-        var data=JSON.parse(request.responseText);
-        addTemp();
-    }
+  var request=new XMLHttpRequest();
+  request.open("GET","xx");
+  request.onload=function(){
+    var data=JSON.parse(request.responseText);
+    addTemp();
+  }
 
 }
 
