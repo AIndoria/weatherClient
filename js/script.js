8fd985f9ace130a1096e675336c56d9ff1b0ca62
AIndoria <indoriaabhishek@gmail.com>
2018-03-30 03:45:13 -0700

    Added Location and API check

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
-function getLocation(){
+var lat=0;
+var lon=0;
+var temperature=document.getElementById("temp");
 
+if (!navigator.geolocation){
+  temperature.innerHTML="Unsupported browser.";
 }
-getLocation();
 
-var temp=document.getElementById("temp");
-
-function loadAPI(){
-  var request=new XMLHttpRequest();
-  request.open("GET","xx");
-  request.onload=function(){
-    var data=JSON.parse(request.responseText);
-    addTemp();
+function getLocation(callback){
+  navigator.geolocation.getCurrentPosition(currentLocation);
+  function currentLocation(position){
+    lat=position.coords.latitude;
+    lon=position.coords.longitude;
+    callback();
   }
-
 }
-
-loadAPI();
+function getAPI(){
+  console.log("getAPI runs!");
+  var request=new XMLHttpRequest();
+  request.open("GET","https://cors-anywhere.herokuapp.com/api.darksky.net/forecast/cadf041a82953661109102821a085bfa/"+lat+","+lon); //Fix:DarkSky people are security twats and won't let me make requests becase unsecure api key. 
+  request.onload=function getRequest(){
+    var data=JSON.parse(request.responseText);
+    console.log(data);
+  }
+  request.send();
+}
+getLocation(getAPI);
